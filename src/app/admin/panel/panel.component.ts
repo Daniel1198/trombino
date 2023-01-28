5ef84a57b1960c152037031146a13e77fa2c895e
unknown <RTI@2PLT-LAP-DCM-JM.SIRTI.ADDS>
2023-01-28 22:25:31 +0000

    Finalisation de la modif des interfaces d'administration

--- src/app/admin/panel/panel.component.ts
+++ src/app/admin/panel/panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-panel',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onLogout() {
+    this.authService.logoutUser();
+    location.reload();
   }
 
 }
